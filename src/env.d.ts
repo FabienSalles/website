5df2d076c9ff62435e639918b0474013e2cec23a
Fabien Salles <github@fabiensalles.com>
2025-02-22 12:19:51 +0100

    chore(feature-flags): disabled pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly TRANSLATION_ENABLED: string;
+  readonly BLOG_ENABLED: string;
+  readonly TRAINING_ENABLED: string;
+  readonly CV_ENABLED: string;
+  readonly ABOUT_ENABLED: string;
+  readonly CONTACT_ENABLED: string;
+  readonly RSS_ENABLED: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
