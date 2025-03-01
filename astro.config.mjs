8c1947db0017a316aafdae94d52845a8e2ccce09
Fabien Salles <github@fabiensalles.com>
2025-03-01 16:33:16 +0100

    chore(seo): avoid redirection with / at the end

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import partytown from '@astrojs/partytown'
 
 export default defineConfig({
   site: 'https://fabiensalles.com',
+  trailingSlash: "never",
   integrations: [
     tailwind(),
     react(),
