b06d9be6db383de12af97497bfbb52d760bb4001
Fabien Salles <contact@fabiensalles.com>
2025-03-03 14:08:31 +0100

    feat(rss): improve it and delete feature flags

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,12 +2,10 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
   readonly TRANSLATION_ENABLED: string;
-  readonly BLOG_ENABLED: string;
   readonly TRAINING_ENABLED: string;
   readonly CV_ENABLED: string;
   readonly ABOUT_ENABLED: string;
   readonly CONTACT_ENABLED: string;
-  readonly RSS_ENABLED: string;
   readonly NEWSLETTER_ENABLED: string;
   readonly GITHUB_BLOG_CONTRIBUTION_BASE_URL: string;
 }
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,23 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-  const posts = await getCollection('blog', ({ data }) => {
-    return import.meta.env.PROD ? !data.draft : true;
-  });
+export async function GET(context: { site: any; }) {
+  const posts = await getCollection('blog');
+
+  const latestPosts = posts
+    .filter((post: any) => !post.data.draft) // Exclure les drafts
+    .sort((a: any, b: any) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime()) // Tri du plus récent au plus ancien
+    .slice(0, 20);
 
   return rss({
     title: 'Fabien Salles - Blog',
     description: 'Articles sur l\'artisanat logiciel, le développement et les bonnes pratiques',
     site: context.site,
-    items: posts.map((post) => ({
+    items: latestPosts.map((post: any) => ({
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.pubDate,
-      link: `/blog/${post.slug}`,
+      link: `/blog/${post.slug}/`,
       categories: post.data.categories,
     })),
     customData: `<language>fr</language>`,
