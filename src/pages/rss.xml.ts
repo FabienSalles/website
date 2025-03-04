cbcd292f8e115d85dcaec3bedf485c6dfa629f81
Fabien Salles <github@fabiensalles.com>
2025-03-04 19:24:52 +0100

    feat(rss): fix rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,21 +5,22 @@ export async function GET(context: { site: any; }) {
   const posts = await getCollection('blog');
 
   const latestPosts = posts
-    .filter((post: any) => !post.data.draft) // Exclure les drafts
-    .sort((a: any, b: any) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime()) // Tri du plus récent au plus ancien
+    .filter((post: any) => !post.data.draft)
+    .map((post: any) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`,
+      categories: post.data.categories,
+    }))
+    .sort((a: any, b: any) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime())
     .slice(0, 20);
 
   return rss({
     title: 'Fabien Salles - Blog',
     description: 'Articles sur l\'artisanat logiciel, le développement et les bonnes pratiques',
     site: context.site,
-    items: latestPosts.map((post: any) => ({
-      title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.pubDate,
-      link: `/blog/${post.slug}/`,
-      categories: post.data.categories,
-    })),
+    items: latestPosts,
     xmlns: {
       atom: "http://www.w3.org/2005/Atom",
     },
