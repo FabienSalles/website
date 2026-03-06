f8b2fd512b5d70fec5ab3a4359b93fffc6a6d9ec
Fabien Salles <contact@fabiensalles.com>
2026-03-06 10:31:52 +0100

    chore(seo): improve schema, title, images and contact form

--- src/components/ImageWithCaption.tsx
+++ src/components/ImageWithCaption.tsx
@@ -6,12 +6,14 @@ interface ImageProps {
   src: string;
   alt: string;
   source: string;
+  width?: number;
+  height?: number;
 }
 
-const ImageWithSource: React.FC<ImageProps> = ({ src, alt, source }) => {
+const ImageWithSource: React.FC<ImageProps> = ({ src, alt, source, width, height }) => {
   return (
         <figure>
-            <img src={src} alt={alt} />
+            <img src={src} alt={alt} width={width} height={height} loading="lazy" />
             <figcaption className="image-source text-right block"><a href={source} rel="nofollow noopener noreferrer">Source de l'image</a></figcaption>
         </figure>
     );
